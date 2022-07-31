547958e79109d5e496105d377184d8d12b827d27
capitanflex <109966942+capitanflex@users.noreply.github.com>
2022-07-31 18:01:16 +0300

    bebra2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForTest : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    //ЕГОР НЕ *****
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // EGOR *****
+    // Update is called once per frame
     void Update()
     {
         
